0b3614ab8175e26b8bcf2b55eb6e6a221347166b
Jason Washburn <wburn@wburn.net>
2021-03-16 13:20:07 -0500

    pulls dad joke and quote, then emails them

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
-# BirthdayWisher
+# Daily Email
+
+A simple script that emails me a motivational quote and a dad joke to torture my son with 😁. 
+
+I plan to automate it so that it runs daily using github actions.
+
+Dad joke is pulled from the [icanhazdadjoke API](https://icanhazdadjoke.com/api)
+
+Motivational quote is pulled from the [zenquotes API](https://zenquotes.io/api)
+
--- main.py
+++ main.py
@@ -1,8 +1,17 @@
+
+# TODO 3. Email Dad joke and quote to myself daily
+# TODO 4. HTML formatting?
+# TODO 5. Configure to use github actions/secrets
+
 import datetime as dt
 import smtplib
 import random
 import config  # store our login info separate from code to keep it out of git
+import requests
+import json
 
+DAD_JOKE_URL = 'https://icanhazdadjoke.com/'
+QUOTES_URL = 'https://zenquotes.io/api/today'
 
 def send_email(to_address, subject, message):
     # Sends an email message using the email server info provided in config.py
@@ -28,3 +37,32 @@ with open("quotes.txt", "r") as quotes_file:
 now = dt.datetime.now()
 if now.weekday() == 1:
     send_email(to_address="wburn.test@yahoo.com", subject="Inspirational Quote", message=random.choice(quotes))
+
+
+def get_dad_joke():
+    headers = {
+        "Accept": "application/json",
+        "user-agent": "WBurn - Daily Email GitHub repo https://github.com/jasonwashburn/DailyEmail"
+    }
+    try:
+        request = requests.get(DAD_JOKE_URL, headers=headers)
+        response = json.loads(request.text)
+        return response
+    except Exception as ex:
+        raise Exception(f"Unable to retrieve dad joke from {DAD_JOKE_URL}: {ex}")
+
+
+def get_quote():
+    try:
+        request = requests.get(QUOTES_URL)
+        response = json.loads(request.text)
+        return response
+    except Exception as ex:
+        raise Exception(f"Unable to retrieve dad joke from {QUOTES_URL}: {ex}")
+
+
+
+print(get_dad_joke()['joke'])
+print(get_quote())
+email_message = f"{get_dad_joke()['joke']}\n\n{get_quote()[0]['q']} - {get_quote()[0]['a']}"
+send_email(to_address=config.target_email, subject="Daily Dad Joke and Quote", message=email_message)
